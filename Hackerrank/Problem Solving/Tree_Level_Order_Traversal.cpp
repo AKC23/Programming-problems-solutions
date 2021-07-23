e5898fd7b11eb88b60231d9fd187c26ace3c9bdb
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2021-07-23 23:31:00 +0600

    Add files via upload

---
+++ Hackerrank/Problem Solving/Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,104 @@
+/*
+Problem's name: Tree: Levelorder Traversal
+Problem's link: https://www.hackerrank.com/challenges/tree-level-order-traversal/problem
+Date: Monday, July 19,2021
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    Node *insert(Node *root, int data)
+    {
+        if (root == NULL)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node *cur;
+            if (data <= root->data)
+            {
+                cur = insert(root->left, data);
+                root->left = cur;
+            }
+            else
+            {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+
+            return root;
+        }
+    }
+    /*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node *root)
+    {
+        queue<Node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            node *P = q.front();
+            q.pop();
+
+            if (P != NULL)
+            {
+                cout << P->data << " ";
+                q.push(P->left);
+                q.push(P->right);
+            }
+        }
+    }
+
+}; //End of Solution
+
+int main()
+{
+
+    Solution myTree;
+    Node *root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while (t-- > 0)
+    {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    myTree.levelOrder(root);
+    return 0;
+}
