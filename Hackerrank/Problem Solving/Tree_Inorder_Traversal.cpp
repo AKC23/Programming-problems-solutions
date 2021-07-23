961eb8ebca55c09e5a6ceff6752bf24f00269f9b
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2021-07-23 23:29:35 +0600

    Add files via upload

---
+++ Hackerrank/Problem Solving/Tree_Inorder_Traversal.cpp
@@ -0,0 +1,105 @@
+/*
+Problem's name: Tree: Inorder Traversal
+Problem's link: https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+Date: Monday, July 19,2021
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    Node *insert(Node *root, int data)
+    {
+        if (root == NULL)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node *cur;
+            if (data <= root->data)
+            {
+                cur = insert(root->left, data);
+                root->left = cur;
+            }
+            else
+            {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+
+            return root;
+        }
+    }
+
+    /* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root)
+    {
+        if (root == NULL)
+            return;
+
+        /* first recur on left child */
+        inOrder(root->left);
+
+        /* then print the data of node */
+        printf("%d ", root->data);
+
+        /* now recur on right child */
+        inOrder(root->right);
+    }
+
+}; //End of Solution
+
+int main()
+{
+
+    Solution myTree;
+    Node *root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while (t-- > 0)
+    {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    myTree.inOrder(root);
+    return 0;
+}
