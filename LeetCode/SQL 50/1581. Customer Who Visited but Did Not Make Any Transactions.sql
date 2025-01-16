cb306485e2badf453061ba3fb7b3d1a774bd3551
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2025-01-17 01:25:25 +0600

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- LeetCode/SQL 50/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ LeetCode/SQL 50/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,4 +1,4 @@
-Problem Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+# Problem Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 
@@ -8,7 +8,3 @@ left join transactions t
 on v.visit_id = t.visit_id
 where t.transaction_id is null
 group by v.customer_id;
-
-
-
-
