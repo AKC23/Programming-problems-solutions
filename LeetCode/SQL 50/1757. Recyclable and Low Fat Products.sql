5e4984df7b20541c335e6a25ca8ef8c438b82e37
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2025-01-17 00:36:01 +0600

    Add files via upload

---
+++ LeetCode/SQL 50/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,5 @@
+Problem Link: https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+
+select product_id from Products 
+where low_fats ='Y' and recyclable = 'Y'
+order by product_id asc;
